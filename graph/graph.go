dc566b3c49dfb351d073b29b5ec445bf44e13750
Filipe Varela <fcvarela@gmail.com>
2016-11-25 12:15:26 +0000

    Make this thread safe

--- graph/graph.go
+++ graph/graph.go
@@ -1,5 +1,7 @@
 package graph
 
+import "sync"
+
 // Handle is an opaque type which represents a graph
 type Handle uint32
 
@@ -14,14 +16,14 @@ type node struct {
 	position     [4]float32
 	velocity     [4]float32
 	acceleration [4]float32
-	custom       [4]float32
+	active       [4]float32
 }
 
 type edge struct {
-	node1ID  NodeHandle
-	node2ID  NodeHandle
-	padding0 uint32
-	padding1 uint32
+	node1ID NodeHandle
+	node2ID NodeHandle
+	active  uint32
+	padding uint32
 }
 
 // graph contains all data necessary to manage a graph where nodes and edges
@@ -40,7 +42,8 @@ type graph struct {
 }
 
 var (
-	graphs []graph
+	graphsLock sync.Mutex
+	graphs     []graph
 )
 
 func init() {
@@ -49,6 +52,9 @@ func init() {
 
 // New returns a handle for a newly created graph
 func New() Handle {
+	graphsLock.Lock()
+	defer graphsLock.Unlock()
+
 	var g = graph{
 		nodes:         make([]node, 0),
 		edges:         make([]edge, 0),
@@ -63,10 +69,16 @@ func New() Handle {
 
 // NewNode returns a handle for a newly created graph node
 func (g Handle) NewNode() NodeHandle {
+	graphsLock.Lock()
+	defer graphsLock.Unlock()
+
 	// do we have any items in our freeset? return the first one after deleting it
 	for k := range graphs[g].freeNodeSet {
 		// delete it from the freeNodeSet
 		delete(graphs[g].freeNodeSet, k)
+
+		// make it active
+		graphs[g].nodes[k].active = [4]float32{1.0, 1.0, 1.0, 1.0}
 
 		// return it
 		return k
@@ -79,37 +91,73 @@ func (g Handle) NewNode() NodeHandle {
 
 // DeleteNode deletes a graph node and all edges connected to or from it
 func (g Handle) DeleteNode(n NodeHandle) {
+	graphsLock.Lock()
+	defer graphsLock.Unlock()
+
+	// make it inactive so the cl kernel and shaders won't use it
+	graphs[g].nodes[n].active = [4]float32{0.0, 0.0, 0.0, 0.0}
+
 	// add the node to the graphs freeNode list so that it can be used
 	// the next time a node is created.
 	graphs[g].freeNodeSet[n] = struct{}{}
+
+	// delete any edge connected to or from this node, they are indexed in nodeEdgeIndex
+	for _, e := range graphs[g].nodeEdgeIndex[n] {
+		g.deleteEdge(e)
+	}
+
+	// reset the index for this node
+	graphs[g].nodeEdgeIndex[n] = graphs[g].nodeEdgeIndex[n][:0]
 }
 
 // NewEdge returns a handle for a newly created graph edge
 func (g Handle) NewEdge(n1, n2 NodeHandle) EdgeHandle {
+	graphsLock.Lock()
+	defer graphsLock.Unlock()
+
+	var newEdge = edge{active: 1, node1ID: n1, node2ID: n2}
+
 	// do we have any items in our freeset? return the first one after deleting it
 	for k := range graphs[g].freeEdgeSet {
 		// remove it from freeEdgeSet
 		delete(graphs[g].freeEdgeSet, k)
 
-		// set it to the passed values
-		graphs[g].edges[k].node1ID = n1
-		graphs[g].edges[k].node2ID = n2
+		// initialize it
+		graphs[g].edges[k] = newEdge
 
-		// return it
+		// done
 		return k
 	}
 
 	// got here? we don't have anything in our free set, add a new edge
-	graphs[g].edges = append(graphs[g].edges, edge{
-		node1ID: n1,
-		node2ID: n2,
-	})
+	graphs[g].edges = append(graphs[g].edges, newEdge)
+
+	// done
 	return EdgeHandle(len(graphs[g].edges) - 1)
+}
+
+func (g Handle) deleteEdge(e EdgeHandle) {
+	// add the edge to the graphs freeNode list so that it can be used
+	// the next time an edge is created.
+	graphs[g].freeEdgeSet[e] = struct{}{}
 }
 
 // DeleteEdge deletes a graph edge
 func (g Handle) DeleteEdge(e EdgeHandle) {
-	// add the edge to the graphs freeNode list so that it can be used
-	// the next time an edge is created.
-	graphs[g].freeEdgeSet[e] = struct{}{}
+	graphsLock.Lock()
+	defer graphsLock.Unlock()
+
+	g.deleteEdge(e)
+}
+
+// indexEdge adds this edge to both node's list of referencing edges
+// this will be checked whenever one of those nodes is deleted to
+// determine what edges need to be removed
+func (g Handle) indexEdge(e EdgeHandle, nodes []NodeHandle) {
+	for _, n := range nodes {
+		if graphs[g].nodeEdgeIndex[n] == nil {
+			graphs[g].nodeEdgeIndex[n] = make([]EdgeHandle, 0)
+		}
+		graphs[g].nodeEdgeIndex[n] = append(graphs[g].nodeEdgeIndex[n], e)
+	}
 }
