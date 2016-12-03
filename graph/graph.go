4ee07046873cfc52b63ad66815e23ce8ac6fbaf4
Filipe Varela <fcvarela@gmail.com>
2016-12-03 12:12:57 +0000

    Use indirect handles so we can reuse indices

--- graph/graph.go
+++ graph/graph.go
@@ -4,16 +4,21 @@
 // and _not_ on inner operations (lowercase).
 package graph
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/satori/go.uuid"
+)
 
 // Handle is an opaque type which represents a graph
-type Handle uint32
+type Handle string
 
 // NodeHandle is an opaque type which represents a graph node
-type NodeHandle uint32
+type NodeHandle string
 
 // EdgeHandle is an opaque type which represents a graph edge
-type EdgeHandle uint32
+type EdgeHandle string
 
 // std140 alignment for opengl/opencl
 type node struct {
@@ -24,10 +29,8 @@ type node struct {
 }
 
 type edge struct {
-	node1ID NodeHandle
-	node2ID NodeHandle
-	active  uint32
-	padding uint32
+	node1Index uint32
+	node2Index uint32
 }
 
 // graph contains all data necessary to manage a graph where nodes and edges
@@ -36,8 +39,10 @@ type edge struct {
 type graph struct {
 	nodes       []node
 	edges       []edge
-	freeNodeSet map[NodeHandle]struct{}
-	freeEdgeSet map[EdgeHandle]struct{}
+	freeNodeSet map[uint32]struct{}
+	freeEdgeSet map[uint32]struct{}
+	nodeHandles map[NodeHandle]uint32
+	edgeHandles map[EdgeHandle]uint32
 
 	// contains a list of edges pointing to and from each node
 	// we use this to delete those edges when a node is deleted
@@ -45,12 +50,20 @@ type graph struct {
 }
 
 var (
-	graphsLock sync.Mutex
-	graphs     []graph
+	graphsLock   sync.Mutex
+	graphs       []graph
+	graphHandles map[Handle]uint32
+)
+
+var (
+	errGraphNotFound = "Graph %s does not exist"
+	errNodeNotFound  = "Node %s does not exist"
+	errEdgeNotFound  = "Edge %s does not exist"
 )
 
 func init() {
 	graphs = make([]graph, 0)
+	graphHandles = make(map[Handle]uint32)
 }
 
 // New returns a handle for a newly created graph
@@ -58,121 +71,199 @@ func New() Handle {
 	graphsLock.Lock()
 	defer graphsLock.Unlock()
 
+	// initialize it
 	var g = graph{
 		nodes:         make([]node, 0),
 		edges:         make([]edge, 0),
-		freeNodeSet:   make(map[NodeHandle]struct{}),
-		freeEdgeSet:   make(map[EdgeHandle]struct{}),
+		nodeHandles:   make(map[NodeHandle]uint32),
+		edgeHandles:   make(map[EdgeHandle]uint32),
+		freeNodeSet:   make(map[uint32]struct{}),
+		freeEdgeSet:   make(map[uint32]struct{}),
 		nodeEdgeIndex: make(map[NodeHandle][]EdgeHandle),
 	}
 
+	// add it to the graph slice
 	graphs = append(graphs, g)
-	return Handle(len(graphs) - 1)
+
+	// create a handle
+	var handle = Handle(uuid.NewV4().String())
+
+	// map it
+	graphHandles[handle] = uint32(len(graphs) - 1)
+
+	return Handle(handle)
 }
 
 // NewNode returns a handle for a newly created graph node
-func (g Handle) NewNode() NodeHandle {
+func NewNode(g Handle) (NodeHandle, error) {
 	graphsLock.Lock()
 	defer graphsLock.Unlock()
 
-	// do we have any items in our freeset? return the first one after deleting it
-	for k := range graphs[g].freeNodeSet {
-		// delete it from the freeNodeSet
-		delete(graphs[g].freeNodeSet, k)
-
-		// make it active
-		graphs[g].nodes[k].active = [4]float32{1.0, 1.0, 1.0, 1.0}
-
-		// return it
-		return k
+	gidx, ok := graphHandles[g]
+	if !ok {
+		return NodeHandle(""), fmt.Errorf(errGraphNotFound, g)
 	}
 
-	// got here? we don't have anything in our free set, add a new node
-	graphs[g].nodes = append(graphs[g].nodes, node{})
-	return NodeHandle(len(graphs[g].nodes) - 1)
+	var nodeIndex uint32
+	var found bool
+
+	// do we have any items in our free index
+	for k := range graphs[gidx].freeNodeSet {
+		delete(graphs[gidx].freeNodeSet, k)
+		found = true
+		nodeIndex = k
+	}
+
+	if !found {
+		nodeIndex = uint32(len(graphs[gidx].nodes))
+		graphs[gidx].nodes = append(graphs[gidx].nodes, node{})
+	}
+
+	// make active
+	graphs[gidx].nodes[nodeIndex].active = [4]float32{1.0, 1.0, 1.0, 1.0}
+
+	// create a handle
+	var handle = NodeHandle(uuid.NewV4().String())
+
+	// map it
+	graphs[gidx].nodeHandles[handle] = nodeIndex
+
+	return handle, nil
 }
 
 // DeleteNode deletes a graph node and all edges connected to or from it
-func (g Handle) DeleteNode(n NodeHandle) {
+func DeleteNode(g Handle, n NodeHandle) error {
 	graphsLock.Lock()
 	defer graphsLock.Unlock()
 
+	// resolve the graph handle
+	gidx, ok := graphHandles[g]
+	if !ok {
+		return fmt.Errorf(errGraphNotFound, g)
+	}
+
+	// resolve the node handle
+	index, ok := graphs[gidx].nodeHandles[n]
+	if !ok {
+		return fmt.Errorf(errNodeNotFound, n)
+	}
+
+	// delete the handle
+	delete(graphs[gidx].nodeHandles, n)
+
 	// make it inactive so the cl kernel and shaders won't use it
-	graphs[g].nodes[n].active = [4]float32{0.0, 0.0, 0.0, 0.0}
+	graphs[gidx].nodes[index].active = [4]float32{0.0, 0.0, 0.0, 0.0}
 
 	// add the node to the graphs freeNode list so that it can be used
 	// the next time a node is created.
-	graphs[g].freeNodeSet[n] = struct{}{}
+	graphs[gidx].freeNodeSet[index] = struct{}{}
 
 	// delete any edge connected to or from this node, they are indexed in nodeEdgeIndex
-	for _, e := range graphs[g].nodeEdgeIndex[n] {
-		g.deleteEdge(e)
+	for _, e := range graphs[gidx].nodeEdgeIndex[n] {
+		deleteEdge(g, e)
 	}
 
 	// reset the index for this node
-	graphs[g].nodeEdgeIndex[n] = graphs[g].nodeEdgeIndex[n][:0]
+	graphs[gidx].nodeEdgeIndex[n] = graphs[gidx].nodeEdgeIndex[n][:0]
+
+	return nil
 }
 
 // NewEdge returns a handle for a newly created graph edge
-func (g Handle) NewEdge(n1, n2 NodeHandle) EdgeHandle {
+func NewEdge(g Handle, nh1, nh2 NodeHandle) (EdgeHandle, error) {
 	graphsLock.Lock()
 	defer graphsLock.Unlock()
 
-	// create the edge
-	var newEdge = edge{active: 1, node1ID: n1, node2ID: n2}
-
-	// do we have any items in our freeset? return the first one after deleting it
-	for k := range graphs[g].freeEdgeSet {
-		// remove it from freeEdgeSet
-		delete(graphs[g].freeEdgeSet, k)
-
-		// initialize it
-		graphs[g].edges[k] = newEdge
-
-		// index it
-		g.indexEdge(k, []NodeHandle{n1, n2})
-
-		// done
-		return k
+	gidx, ok := graphHandles[g]
+	if !ok {
+		return EdgeHandle(""), fmt.Errorf(errGraphNotFound, g)
 	}
 
-	// got here? we don't have anything in our free set, add a new edge
-	graphs[g].edges = append(graphs[g].edges, newEdge)
+	var edgeIndex uint32
+	var found bool
+
+	// do we have any items in our free index
+	for k := range graphs[gidx].freeEdgeSet {
+		delete(graphs[gidx].freeEdgeSet, k)
+		found = true
+		edgeIndex = k
+	}
+
+	if !found {
+		edgeIndex = uint32(len(graphs[gidx].edges))
+		graphs[gidx].edges = append(graphs[gidx].edges, edge{})
+	}
+
+	// resolve n1 and n2
+	n1, ok := graphs[gidx].nodeHandles[nh1]
+	if !ok {
+		return EdgeHandle(""), fmt.Errorf(errNodeNotFound, nh1)
+	}
+
+	n2, ok := graphs[gidx].nodeHandles[nh2]
+	if !ok {
+		return EdgeHandle(""), fmt.Errorf(errNodeNotFound, nh2)
+	}
+
+	// set it
+	graphs[gidx].edges[edgeIndex] = edge{node1Index: n1, node2Index: n2}
 
 	// get a handle
-	var newEdgeHandle = EdgeHandle(len(graphs[g].edges) - 1)
+	var handle = EdgeHandle(uuid.NewV4().String())
+
+	// map it
+	graphs[gidx].edgeHandles[handle] = edgeIndex
 
 	// index it
-	g.indexEdge(newEdgeHandle, []NodeHandle{n1, n2})
+	indexEdge(gidx, handle, []NodeHandle{nh1, nh2})
 
-	return newEdgeHandle
+	return handle, nil
 }
 
-func (g Handle) deleteEdge(e EdgeHandle) {
+func deleteEdge(g Handle, e EdgeHandle) error {
+	// resolve graph
+	gidx, ok := graphHandles[g]
+	if !ok {
+		return fmt.Errorf(errGraphNotFound, g)
+	}
+
+	// resolve edge
+	eidx, ok := graphs[gidx].edgeHandles[e]
+	if !ok {
+		return fmt.Errorf(errEdgeNotFound, e)
+	}
+
+	// delete the edge
+	delete(graphs[gidx].edgeHandles, e)
+
 	// add the edge to the graphs freeNode list so that it can be used
 	// the next time an edge is created.
-	graphs[g].freeEdgeSet[e] = struct{}{}
+	graphs[gidx].freeEdgeSet[eidx] = struct{}{}
+
+	// it will still end up in the gpu so make it a zero-length line
+	graphs[gidx].edges[eidx].node1Index = 0
+	graphs[gidx].edges[eidx].node2Index = 0
+
+	return nil
 }
 
 // DeleteEdge deletes a graph edge
-func (g Handle) DeleteEdge(e EdgeHandle) {
+func DeleteEdge(g Handle, e EdgeHandle) error {
 	graphsLock.Lock()
 	defer graphsLock.Unlock()
 
-	g.deleteEdge(e)
+	return deleteEdge(g, e)
 }
 
 // indexEdge adds this edge to both node's list of referencing edges
 // this will be checked whenever one of those nodes is deleted to
 // determine what edges need to be removed
-func (g Handle) indexEdge(e EdgeHandle, nodes []NodeHandle) {
+func indexEdge(gidx uint32, e EdgeHandle, nodes []NodeHandle) {
 	for _, n := range nodes {
-		// does the slice exist?
-		if graphs[g].nodeEdgeIndex[n] == nil {
-			// nope, create it
-			graphs[g].nodeEdgeIndex[n] = make([]EdgeHandle, 0)
+		if graphs[gidx].nodeEdgeIndex[n] == nil {
+			graphs[gidx].nodeEdgeIndex[n] = make([]EdgeHandle, 0)
 		}
 		// append the edge to it
-		graphs[g].nodeEdgeIndex[n] = append(graphs[g].nodeEdgeIndex[n], e)
+		graphs[gidx].nodeEdgeIndex[n] = append(graphs[gidx].nodeEdgeIndex[n], e)
 	}
 }
